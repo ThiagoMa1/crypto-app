9a0dad34ff956dafa859da2b724a54b80fa3fb35
Thiago <thiagomandrade15@gmail.com>
2022-08-31 16:17:18 -0300

    changing the data for the options, also creating the getData function

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,32 +1,47 @@
 import { FC, useEffect, useState } from "react";
 import Coins from "../components/Coins";
-import cc from "currency-codes";
 
 import "./Home.styles.scss";
 
 const Home: FC = () => {
   document.title = "Crypto Checker";
   const [coins, setCoins] = useState([]);
+  const [currencies, setCurrencies] = useState<string[]>([]);
   const [searchValue, setSearchValue] = useState("");
   const [userCurrency, setUserCurrency] = useState("brl");
 
   useEffect(() => {
-    fetch(
-      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${userCurrency}&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        setCoins(data);
-      });
+    getData(
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${userCurrency}&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h`,
+      true
+    );
+    if (!currencies.length)
+      getData(
+        "https://api.coingecko.com/api/v3/simple/supported_vs_currencies"
+      );
+
+    try {
+    } catch (error) {}
   }, [userCurrency]);
+
+  const getData = async (url: string, coins?: boolean) => {
+    const req = await fetch(url);
+    const data = await req.json();
+    console.log(data);
+    if (coins) {
+      setCoins(data);
+    } else {
+      setCurrencies(data);
+    }
+  };
 
   const searchResults = coins.filter((coin: any) =>
     coin.name.toLowerCase().includes(searchValue.toLowerCase())
   );
 
-  const curatedCurrencyCodes = cc
-    .codes()
-    .filter((curr) => !curr.toLowerCase().includes("brl"));
+  const curatedCurrencyCodes = currencies.filter(
+    (curr) => !curr.toLowerCase().includes("brl")
+  );
   curatedCurrencyCodes.unshift("BRL");
 
   return (
@@ -38,8 +53,12 @@ const Home: FC = () => {
       <div className="currency">
         <div>
           <select onChange={(e) => setUserCurrency(e.target.value)}>
-            {curatedCurrencyCodes.map((currency) => (
-              <option key={currency} value={currency} label={currency} />
+            {curatedCurrencyCodes.map((currency: string) => (
+              <option
+                key={currency}
+                value={currency}
+                label={currency.toUpperCase()}
+              />
             ))}
           </select>
           <input
